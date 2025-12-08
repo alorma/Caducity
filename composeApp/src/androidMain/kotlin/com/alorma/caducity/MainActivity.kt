c5dbb01ae601f003f21dbad1eb251f6be9fabb36
Bernat Borrás <bernatbor15@gmail.com>
2025-12-08 16:27:10 +0100

    ??

--- composeApp/src/androidMain/kotlin/com/alorma/caducity/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/alorma/caducity/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.alorma.caducity.settings.initializeSettings
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    initializeSettings(this)
     enableEdgeToEdge()
 
     setContent {
