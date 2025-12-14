3b3c36a29b1e1d4fc09dea2f7c4aa5fe2a0fdb38
Bernat Borrás <bernatbor15@gmail.com>
2025-12-14 13:28:04 +0100

    Missing import

--- composeApp/src/commonMain/kotlin/com/alorma/caducity/App.kt
+++ composeApp/src/commonMain/kotlin/com/alorma/caducity/App.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
+import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
