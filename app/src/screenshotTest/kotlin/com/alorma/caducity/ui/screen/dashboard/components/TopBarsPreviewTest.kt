d2839ee8afbc84d3877228291c53ec7614bd76a2
Bernat Borrás <bernatbor15@gmail.com>
2026-01-01 22:03:05 +0100

    UPdates

--- app/src/screenshotTest/kotlin/com/alorma/caducity/ui/screen/dashboard/components/TopBarsPreviewTest.kt
+++ app/src/screenshotTest/kotlin/com/alorma/caducity/ui/screen/dashboard/components/TopBarsPreviewTest.kt
@@ -1,17 +1,20 @@
 package com.alorma.caducity.ui.screen.dashboard.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.alorma.caducity.base.ui.components.StyledCenterAlignedTopAppBarPreview
 import com.alorma.caducity.base.ui.components.StyledTopAppBarPreview
 import com.android.tools.screenshot.PreviewTest
 
 @PreviewTest
+@Preview
 @Composable
 fun StyledTopBarTest() {
   StyledTopAppBarPreview()
 }
 
 @PreviewTest
+@Preview
 @Composable
 fun StyledCenterAlignedTest() {
   StyledCenterAlignedTopAppBarPreview()
