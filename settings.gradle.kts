870414dbee80f3750f27d11868bd9af03176adda
Bernat Borrás <bernatbor15@gmail.com>
2026-01-01 20:44:34 +0100

    Remove things

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,10 +18,6 @@ dependencyResolutionManagement {
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
 include(":app")
 include(":base")
 include(":base:main")
