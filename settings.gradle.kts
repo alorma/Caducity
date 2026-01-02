7598ec2c51ac45c802ea9b5ed5c76d41e0454504
Bernat Borrás <bernatbor15@gmail.com>
2026-01-02 12:28:57 +0100

    Clean gradle files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,21 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-        mavenLocal()
-    }
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+    mavenLocal()
+  }
 }
 
 dependencyResolutionManagement {
-    repositories {
-        google()
-        mavenCentral()
-        mavenLocal()
-        maven("https://jitpack.io")
-    }
-}
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+  repositories {
+    google()
+    mavenCentral()
+    mavenLocal()
+    maven("https://jitpack.io")
+  }
 }
 
 include(":app")
