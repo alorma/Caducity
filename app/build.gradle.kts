61690ed2000e0109d496f212dbb4fbb3ae9714cf
Bernat Borrás <bernatbor15@gmail.com>
2026-01-04 00:49:26 +0100

    Java 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,10 +23,6 @@ android {
     versionCode = 1
     versionName = "1.0"
   }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-  }
   packaging {
     resources {
       excludes += "/META-INF/{AL2.0,LGPL2.1}"
