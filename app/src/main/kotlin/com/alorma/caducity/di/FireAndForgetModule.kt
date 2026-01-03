d803c87033326e8eb011c659555539484b9fb7a3
Bernat Borrás <bernatbor15@gmail.com>
2026-01-03 01:23:30 +0100

    Update fire and forget dependency

--- app/src/main/kotlin/com/alorma/caducity/di/FireAndForgetModule.kt
+++ app/src/main/kotlin/com/alorma/caducity/di/FireAndForgetModule.kt
@@ -1,7 +1,7 @@
 package com.alorma.caducity.di
 
 import com.alorma.fireandforget.FireAndForgetRunner
-import com.alorma.fireandforget.mukltiplatform.settings.SettingsFireAndForgetRunner
+import com.alorma.fireandforget.multiplatform.settings.SettingsFireAndForgetRunner
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
