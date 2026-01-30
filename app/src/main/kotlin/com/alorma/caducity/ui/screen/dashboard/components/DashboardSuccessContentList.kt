804e37d98e3480d021380eff9701e5a9790b1943
Bernat Borrás <bernatbor15@gmail.com>
2026-01-30 19:34:14 +0100

    Add clickable

--- app/src/main/kotlin/com/alorma/caducity/ui/screen/dashboard/components/DashboardSuccessContentList.kt
+++ app/src/main/kotlin/com/alorma/caducity/ui/screen/dashboard/components/DashboardSuccessContentList.kt
@@ -1,20 +1,29 @@
 package com.alorma.caducity.ui.screen.dashboard.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import com.alorma.caducity.base.ui.icons.AppIcons
+import com.alorma.caducity.base.ui.icons.outlined.CircleChevronRight
 import com.alorma.caducity.domain.model.InstanceStatus
 import com.alorma.caducity.ui.components.calendar.CaducityWeekCalendar
 import com.alorma.caducity.ui.screen.dashboard.DashboardState
 import com.alorma.caducity.ui.theme.CaducityTheme
-import kotlinx.datetime.LocalDate
 
 @Composable
 fun DashboardSuccessContentList(
@@ -53,10 +62,28 @@ fun DashboardSuccessContentList(
           Column(
             modifier = Modifier.animateItem(),
           ) {
-            Text(
-              text = productCalendarState.name,
-              style = CaducityTheme.typography.titleMedium,
-            )
+            Row(
+              modifier = Modifier
+                .fillMaxWidth()
+                .clip(CaducityTheme.shapes.small)
+                .clickable {
+                  onNavigateToProduct(productCalendarState.id)
+                }
+                .padding(horizontal = 4.dp, vertical = 8.dp),
+              verticalAlignment = Alignment.CenterVertically,
+            ) {
+              Text(
+                modifier = Modifier.padding(start = 8.dp).weight(1f),
+                text = productCalendarState.name,
+                style = CaducityTheme.typography.titleMedium,
+              )
+
+              Icon(
+                modifier = Modifier.padding(end = 8.dp).size(18.dp),
+                imageVector = AppIcons.Outlined.CircleChevronRight,
+                contentDescription = null,
+              )
+            }
 
             CaducityWeekCalendar(
               appCalendarConfig = productCalendarState.appCalendarConfig,
