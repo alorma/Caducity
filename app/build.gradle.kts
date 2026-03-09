cfd3dcb1a00b360b035d1eb11fc13b7275735e81
Bernat Borrás <bernatbor15@gmail.com>
2026-03-09 19:45:51 +0100

    Ui tiles to 3.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -148,8 +148,6 @@ dependencies {
   implementation(libs.multiplatform.settings.no.arg)
   implementation(libs.fireandforget.core)
   implementation(libs.fireandforget.multiplatform.settings)
-  implementation(libs.alorma.settings.ui.tiles)
-  implementation(libs.alorma.settings.ui.tiles.extended)
   implementation(libs.alorma.settings.ui.tiles.expressive)
 
   // Koin
